adc567dc3ea3b6c62f597dc259768e30c513ce70
Jsgtaj <jasaj316@gmail.com>
2020-03-15 19:33:16 -0400

    changed placeholders to more accurate versions.

--- register.php
+++ register.php
@@ -7,12 +7,13 @@ require "header.php";
   <fieldset>
     <legend>User Info:</legend>
     <label for="username">Username:</label>
-    <input name="username" type="email" placeholder="Username" required>
+    <input name="username" type="email" placeholder="user@domain.com" required>
     <label for="password">Password:</label>
-    <input name="password" type="password" placeholder="Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
+    <input name="password" type="password" placeholder="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
     <label for="passwordConfirm">Confirm your Password:</label>
-    <input name="passwordConfirm" type="password" placeholder="Confirm Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
-    <p>Password needs at least one number, one lowercase letter,<br> one uppercase letter, and has to be at least 8 characters long.</p>
+    <input name="passwordConfirm" type="password" placeholder="confirm password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
+    <p>Passwords need at least one number, one lowercase letter,<br>
+      one uppercase letter, and have to be at least 8 characters long.</p>
     <button type="submit">Sign up</button>
   </fieldset>
 </form>
