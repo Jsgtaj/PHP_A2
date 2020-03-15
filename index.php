0f7a3e792dcb18c177f603c742ff8d3343ee144a
Jsgtaj <jasaj316@gmail.com>
2020-03-15 18:11:19 -0400

    Finished simple styling of part 1.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require "header.php";
 ?>
 
 <h1>Welcome to the PHP Web Builder!</h1>
-<p><a href="register.php">Register</a> or <a href="login.php">login</a> to begin making your very own website!</p>
+<p><a href="register.php">Register</a> or <a href="login.php">log in</a> to begin making your very own website!</p>
 
 <?php
 require "footer.php";
