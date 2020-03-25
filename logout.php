a1917dd22f871f5852a4c64e6e726cf559766b69
Jsgtaj <jasaj316@gmail.com>
2020-03-25 16:58:20 -0400

    added style flexibility for additional pages, logout now directs back to login.

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();
 session_destroy();
-header("location:index.php");
+header("location:login.php");
 //opens the session, unsets the variable, destroys section, links back to index
