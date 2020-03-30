f4e13e2b4c595aba4f2e149dd9064c73e67b029c
Jsgtaj <jasaj316@gmail.com>
2020-03-30 19:47:18 -0400

    changed admins table to css grid format.

--- admins.php
+++ admins.php
@@ -6,33 +6,27 @@ require "header.php";
 require "session-auth.php";
 //checks if user is allowed to go here
 ?>
-<table>
-  <thead>
-    <tr>
-      <th scope="col">Admin</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-    require "db.php";
-    $sql = "SELECT * FROM cms_users";
-    $cmd = $db->prepare($sql);
-    $cmd->execute();
-    $arr = $cmd->fetchAll();
-    //gets an array of all data from cms_users
-    foreach ($arr as $admin) {
-      echo "</tr><td>" . $admin['username'] . "</td>";
-      echo "<td><a href=edit-admin.php?id=" . $admin['userId'] . ">Edit</a></td>";
-      echo '<td><a onclick="return confirmDelete(\'' . $admin["username"] . '\',\'';
-      echo $admin['userId'] . '\',\'' . $_SESSION["userId"] . '\');"';
-      echo "href=delete-admin.php?user=" . $admin['userId'] . ">Delete</a><tr>";
-    } //echo all the usernames plus edit and delete links with appropriate variables.
-    $db = null;
-    ?>
-  </tbody>
-</table>
+<div class="grid">
+  <div>Admin Name</div>
+  <div>Edit Admin</div>
+  <div>Delete Admin</div>
+  <?php
+  require "db.php";
+  $sql = "SELECT * FROM cms_users";
+  $cmd = $db->prepare($sql);
+  $cmd->execute();
+  $arr = $cmd->fetchAll();
+  //gets an array of all data from cms_users
+  foreach ($arr as $admin) {
+    echo "<div>" . $admin['username'] . "</div>";
+    echo "<div><a href=edit-admin.php?id=" . $admin['userId'] . ">Edit</a></div>";
+    echo '<div><a onclick="return confirmDelete(\'' . $admin["username"] . '\',\'';
+    echo $admin['userId'] . '\',\'' . $_SESSION["userId"] . '\');"';
+    echo "href=delete-admin.php?user=" . $admin['userId'] . ">Delete</a></div>";
+  } //echo all the usernames plus edit and delete links with appropriate variables.
+  $db = null;
+  ?>
+</div>
 <script>
   function confirmDelete(username, currentId, sessionId) {
     if (currentId == sessionId) {
