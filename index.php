5f2dbea52536e8a0dca0ca6ff2cf22749a0fed88
Jsgtaj <jasaj316@gmail.com>
2020-04-06 14:18:24 -0400

    fixed id numbers on index page when id is not defined

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require "db.php";
 $pageId = $_GET["id"];
+if (empty($pageId)) {
+  //if user went to an index page without an id, default id to 1
+  header("location:index.php?id=1");
+}
 $sql = "SELECT * FROM cms_pages WHERE pageId = :pageId";
 $cmd = $db->prepare($sql);
 $cmd->bindParam(":pageId", $pageId, PDO::PARAM_INT);
@@ -14,6 +18,8 @@ if (!empty($arr)) {
   //setting title and id variables for the header
   require "header.php";
   // Adding header  
+  $_SESSION["panel"] = false;
+  //Hide control panel when going to an index page
   $contentFormatted = preg_replace("/
 /", "<br>", $content);
   //replacing enters with <br>
@@ -24,6 +30,8 @@ if (!empty($arr)) {
   //If there are no pages, show nothing
   $title = "Blank Page";
   require "header.php";
+  $_SESSION["panel"] = false;
+  //Hide control panel when going to an index page
 }
 require "footer.php";
 // Adding footer
